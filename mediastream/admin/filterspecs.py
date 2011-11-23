2f5ab0eddf857f7bf39060f7fc869334448eaa03
Ryan Tucker <rtucker@gmail.com>
2011-11-23 16:31:51 -0500

    filterspecs: Fix multi-table inheritance, clean

--- mediastream/admin/filterspecs.py
+++ mediastream/admin/filterspecs.py
@@ -1,7 +1,7 @@
 # Authors: Marinho Brandao <marinho at gmail.com>
 #          Guilherme M. Gondim (semente) <semente at taurinus.org>
 # File: <your project>/admin/filterspecs.py
-# From http://djangosnippets.org/snippets/1051/
+# From http://djangosnippets.org/snippets/1051/, with modifications
 
 from django.db import models
 from django.contrib.admin.filterspecs import FilterSpec, ChoicesFilterSpec
@@ -17,32 +17,45 @@ class AlphabeticFilterSpec(ChoicesFilterSpec):
     my_model_field.alphabetic_filter = True
     """
 
-    def __init__(self, f, request, params, model, model_admin, field_path=None):
+    def __init__(self, f, request, params, model, model_admin,
+                 field_path=None):
         super(AlphabeticFilterSpec, self).__init__(f, request, params, model,
                                           model_admin, field_path=field_path)
-        self.field_model = f.model
-        self.admin_model = model
-        self.lookup_kwarg = '%s__istartswith' % f.name
+        if f.model != model and f.model not in model._meta.parents:
+            # Filtering by relation
+            self.lookup = '%s__%s' % (f.model._meta.module_name, f.name)
+            self.words = '%s %s' % (f.model._meta.verbose_name, f.verbose_name)
+            values_list = f.model.objects.values_list(f.name, flat=True)
+        elif f.model in model._meta.parents:
+            # Field is inherited from a parent
+            self.lookup = f.name
+            self.words = '%s %s' % (model._meta.verbose_name, f.verbose_name)
+            values_list = model.objects.values_list(f.name, flat=True)
+        else:
+            self.lookup = f.name
+            self.words = f.verbose_name
+            values_list = model.objects.values_list(f.name, flat=True)
+        self.lookup_kwarg = '%s__istartswith' % self.lookup
         self.lookup_val = request.GET.get(self.lookup_kwarg, None)
-        values_list = model.objects.values_list(f.name, flat=True)
         # getting the first char of values
-        self.lookup_choices = list(set(val[0].upper() for val in values_list if val))
-        self.lookup_choices.sort()
+        self.lookup_choices = sorted(list(
+            set(val[0].upper() for val in values_list if val)))
 
     def choices(self, cl):
-        yield {'selected': self.lookup_val is None,
-                'query_string': cl.get_query_string({}, [self.lookup_kwarg]),
-                'display': _('All')}
+        yield {
+            'selected': self.lookup_val is None,
+            'query_string': cl.get_query_string({}, [self.lookup_kwarg]),
+            'display': _('All')}
         for val in self.lookup_choices:
-            yield {'selected': smart_unicode(val) == self.lookup_val,
-                    'query_string': cl.get_query_string({self.lookup_kwarg: val}),
-                    'display': val.upper()}
+            yield {
+                'selected': smart_unicode(val) == self.lookup_val,
+                'query_string': cl.get_query_string({self.lookup_kwarg: val}),
+                'display': val.upper()}
+
     def title(self):
-        return _('%(model_name)s%(field_name)s that starts with') % \
-            {'field_name': self.field.verbose_name,
-             'model_name': self.field_model._meta.verbose_name + ' '
-                if self.field_model != self.admin_model else ''}
+        return _('%s that starts with') % self.words
 
 # registering the filter
-FilterSpec.filter_specs.insert(0, (lambda f: getattr(f, 'alphabetic_filter', False),
-                                   AlphabeticFilterSpec))
+FilterSpec.filter_specs.insert(0,
+    (lambda f: getattr(f, 'alphabetic_filter', False),
+    AlphabeticFilterSpec))
