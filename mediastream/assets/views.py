99c43bbcba1efe3b58f7fdb97ea531b659043419
Ryan Tucker <rtucker@gmail.com>
2011-12-18 14:53:03 -0500

    improvements to file uploading

--- mediastream/assets/views.py
+++ mediastream/assets/views.py
@@ -5,27 +5,37 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from mediastream.assets.forms import UploadFileForm
+from mediastream.assets.forms import UploadFileForm, ImportFileForm
 from mediastream.assets.models import Album, Artist, Track, AssetFile
 from mediastream.utilities.mediainspector import Inspector
 
+import os
 from tempfile import NamedTemporaryFile
 import zipfile
 
 @login_required
 def upload_file(request):
+    # TODO: Much of this can be eliminated, thanks to Track.objects.create_from_file
     if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            stash = NamedTemporaryFile()
-            inspfp = NamedTemporaryFile()
-            for chunk in request.FILES['file'].chunks():
-                stash.write(chunk)
-                inspfp.write(chunk)
+        upform = UploadFileForm(request.POST, request.FILES)
+        imform = ImportFileForm(request.POST)
+        stashes = []
+        if upform.is_valid():
+            for upload in request.FILES.getlist('file'):
+                stash = NamedTemporaryFile()
+                for chunk in upload.chunks():
+                    stash.write(chunk)
+                stash.seek(0)
+                stashes.append((stash, upload.name,))
 
+        if imform.is_valid() and 'path' in request.POST:
+            stashes.append((open(request.POST.get('path')), request.POST.get('path'),))
+
+        for stash, filename in stashes:
+            inspfp = NamedTemporaryFile(suffix=os.path.splitext(filename)[1])
+            inspfp.write(stash.read())
             stash.seek(0)
             inspfp.seek(0)
-
             inspobj = Inspector(fileobj=inspfp)
             files = []
 
@@ -46,13 +56,13 @@ def upload_file(request):
                     mytarget.seek(0)
                     files.append((mytarget, myinspobj))
                     count += 1
-                messages.info(request, 'Found {0} file{1} in zip file {2}.'.format(count, '' if count == 1 else 's', request.FILES['file'].name))
+                messages.info(request, 'Found {0} file{1} in zip file {2}.'.format(count, '' if count == 1 else 's', filename))
             elif inspobj.mimetype.startswith('audio/'):
                 stash.seek(0)
                 files.append((stash, inspobj))
-                messages.info(request, 'Found {0} to be a Track of type {1}.'.format(request.FILES['file'].name, inspobj.mimetype))
+                messages.info(request, 'Found {0} to be a Track of type {1}.'.format(filename, inspobj.mimetype))
             else:
-                messages.error(request, 'Could not figure out what to do with {0} of type {1}.'.format(request.FILES['file'].name, inspobj.mimetype))
+                messages.error(request, 'Could not figure out what to do with {0} of type {1}.'.format(filename, inspobj.mimetype))
 
             for f, i in files:
                 count = 0
@@ -106,5 +116,6 @@ def upload_file(request):
                 count += 1
             return HttpResponseRedirect('/assets/upload/')
     else:
-        form = UploadFileForm()
-    return render_to_response('assets/upload.html', {'form': form}, context_instance=RequestContext(request))
+        upform = UploadFileForm()
+        imform = ImportFileForm()
+    return render_to_response('assets/upload.html', {'upform': upform, 'imform': imform}, context_instance=RequestContext(request))
