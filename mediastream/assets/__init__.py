e90a0e7b1c237c31d6573865a85170d333b8c7e4
Ryan Tucker <rtucker@gmail.com>
2012-12-09 19:23:12 -0500

    Make S3 backend filenames slightly more unique
    This will hopefully help prevent an unfortunate circumstance when
    we go to delete the backend file for one assetfile, which happens
    to have the same filename as another...

--- mediastream/assets/__init__.py
+++ mediastream/assets/__init__.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from utilities.mediainspector import mt
 
 import os
+import time
 
 basepath = getattr(settings, 'ASSETS_UPLOAD_TO', '/assets')
 
@@ -10,17 +11,23 @@ def _get_upload_path(instance, filename):
     "Determine the location for uploaded files."
     if not instance.mimetype:
         instance.mimetype = mt.guess_type(filename)[0]
-    
+        instance.save()
+
+    # Make filename slightly more unique
+    inst_base, inst_ext = os.path.splitext(instance.filename)
+    inst_base += '_%x' % (time.time()*100)
+    inst_fn = inst_base + inst_ext
+
     if hasattr(instance.asset, 'track'):
         if instance.asset.track.album.is_compilation:
             return os.path.join(basepath,
                     'V',
                     'Various Artists',
-                    instance.asset.track.album.name, instance.filename)
+                    instance.asset.track.album.name, inst_fn)
         else:
             return os.path.join(basepath,
                     instance.asset.track.artist.name.upper()[0],
                     instance.asset.track.artist.name,
-                    instance.asset.track.album.name, instance.filename)
+                    instance.asset.track.album.name, inst_fn)
     else:
-        return os.path.join(basepath, instance.filename)
+        return os.path.join(basepath, inst_fn)
